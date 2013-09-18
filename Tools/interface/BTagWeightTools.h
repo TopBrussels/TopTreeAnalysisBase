f1339b66050ce2f2f85c3cc8b26d1639f06c87a1
gvonsem <gvonsem@vub.ac.be>
2013-09-18 13:14:35 +0200

    Update for EPS2013 recommendation (for 2012 rereco data) + adding functions for a SF-application method

--- Tools/interface/BTagWeightTools.h
+++ Tools/interface/BTagWeightTools.h
@@ -9,7 +9,7 @@
 
 Unfortunately the BTV POG does not give the parameterizations in a parsable format that is consistent even per measurements, so you need to edit the files yourself to the level that they are usable. THe parsing code currently is tuned for the Moriond 2012 parameterizations from ttbar+mujet file
 
-Fake rates: Now also using the Fake Rate parameterizations that are documented (for Moriond 2013) here: https://twiki.cern.ch/twiki/pub/CMS/BtagPOG/SFlightFuncs_Moriond2013.C
+Fake rates: Now also using the Fake Rate parameterizations that are documented (for Moriond 2013) here: https://twiki.cern.ch/twiki/pub/CMS/BtagPOG/SFlightFuncs_Moriond2013.C or (for EPS 2013) here: https://twiki.cern.ch/twiki/pub/CMS/BtagPOG/SFlightFuncs_EPS2013.C
 As this is a bare Root Macro this has been c++ized into a separate set of functions.
 
 
@@ -40,6 +40,8 @@ As this is a bare Root Macro this has been c++ized into a separate set of functi
 #include <cmath>
 
 #include <TF1.h>
+#include <TH2F.h>
+#include <TFile.h>
 #include <TString.h>
 
 using namespace std;
@@ -52,6 +54,7 @@ class BTagWeightTools
 
   // constructors, destructors
   BTagWeightTools();
+	BTagWeightTools(std::string filename,std::string defaultalgo,std::string sflightprescription); //sflightprescription default set to "EPS2013", but can be set to "MORIONDS2013"
   BTagWeightTools(std::string filename,std::string defaultalgo);
   BTagWeightTools(std::string filename);
   ~BTagWeightTools();
@@ -64,15 +67,23 @@ class BTagWeightTools
   void setABCDRangeFakeRates(TString range){_abcdrange=range;} // set to "ABCD" for default. Consult BTV documentation for options (essentially "ABCD","AB","C","D")
 
   // useful getters
-  float getWeight(float pt, float eta, int flavor,int syst);
-  float getWeight(float pt, float eta, int flavor,string algo,int syst);
-  //  float getWeight(TRootJet *jet,int syst=0); // uses getWeight(float pt, float eta,int flavor,int syst=0);
-  //  float getWeight(TRootJet *jet,string algo,int syst=0); // uses getWeight(float pt, float eta,int flavor,int syst=0);
+  float getSF(float pt, float eta, int flavor,int syst);
+  float getSF(float pt, float eta, int flavor,string algo,int syst);
+  //  float getSF(TRootJet *jet,int syst=0); // uses getSF(float pt, float eta,int flavor,int syst=0);
+  //  float getSF(TRootJet *jet,string algo,int syst=0); // uses getSF(float pt, float eta,int flavor,int syst=0);
 
   float getUncertainty(float pt, float eta, int flavor, int syst);
   float getUncertainty(float pt, float eta, int flavor, string algo, int syst);
   //  float getUncertainty(float pt, float eta, int flavor, int syst); // uses getUncertainty (float pt, float eta, int flavor, int syst);
   //  float getUncertainty(float pt, float eta, int flavor, string algo, int syst);  // uses getUncertainty (float pt, float eta, int flavor, int syst);
+
+  void InitializeMCEfficiencyHistos(int NofPtBins=15,float PtMin=30.,float PtMax=340.,int NofEtaBins=2);  //the binning is more or less arbitrary; but you should have enough statistics in each bin
+  void FillMCEfficiencyHistos(vector< TRootJet* >& selectedJets);
+	void FillMCEfficiencyHistos(vector< TLorentzVector >& selectedJets, vector<int>& selectedJets_partonFlavour, vector<float>& selectedJets_bTagValues);
+	void WriteMCEfficiencyHistos(std::string outfilename);
+	void ReadMCEfficiencyHistos(std::string infilename);
+	float getMCEventWeight(vector< TRootJet* >& selectedJets, int syst);
+  float getMCEventWeight(vector< TLorentzVector >& selectedJets, vector<int>& selectedJets_partonFlavour, vector<float>& selectedJets_bTagValues, int syst);
 
   // other getters
   std::string getCurrentFile()       {return _filename;} // returns path to currently used file
@@ -104,14 +115,28 @@ class BTagWeightTools
   float _ptmax;
   float _etamax;
   std::string _defaultalgo;
-
+  std::string _sflightprescription;
+	
   TString _abcdrange;
 
-  TF1* fillfakerates(TString meanminmax, TString tagger, TString TaggerStrength, Float_t Etamin, Float_t Etamax, TString DataPeriod);
+  TF1* fillfakerates(TString meanminmax, TString tagger, TString TaggerStrength, Float_t Etamin, Float_t Etamax, TString DataPeriod, TString Prescription);
   std::pair<float,float> getfakeraterange(TString tagger, float eta);
-  TString makefakeratename(TString meanminmax, TString tagger, TString TaggerStrength, Float_t Etamin, Float_t Etamax, TString DataPeriod);
+  TString makefakeratename(TString meanminmax, TString tagger, TString TaggerStrength, Float_t Etamin, Float_t Etamax, TString DataPeriod, TString Prescription);
   std::map<TString,TF1*> _fakeratesfunctions;
-
+	
+	void setalgoWPcut(std::string algo);
+	map<string,TH2F*> _histo2D;
+	bool MCEfficiencyHistosInitialized;
+	float _PtMaxhistos;
+	float _algoWPcut;
+	TH2F *inBtaggedJets;
+	TH2F *inBtaggedBJets;
+	TH2F *inBtaggedCJets;
+	TH2F *inBtaggedLightJets;
+	TH2F *inTotalNofBJets;
+	TH2F *inTotalNofCJets;
+	TH2F *inTotalNofLightJets;
+	
 };
 
 #endif
