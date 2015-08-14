47f87f5147302050b5f301e0f77269664f158b7b
Annik <annik.olbrechts@cern.ch>
2015-08-14 19:37:04 +0200

    LinkDef and Dict specifically for anomalous couplings project

---
+++ AnomCoupLinkDef.h
@@ -0,0 +1,26 @@
+#ifdef __CINT__
+#include "../AnomalousCouplings/PersonalClasses/interface/BTagStudy.h"
+#include "../AnomalousCouplings/PersonalClasses/interface/LHCOOutput.h"
+#include "../AnomalousCouplings/PersonalClasses/interface/MlbStudy.h"
+#include "../AnomalousCouplings/PersonalClasses/interface/TFCreation.h"
+#include "../AnomalousCouplings/PersonalClasses/interface/AnomCoupLight.h"
+#else
+#include "../AnomalousCouplings/PersonalClasses/interface/BTagStudy.h"
+#include "../AnomalousCouplings/PersonalClasses/interface/LHCOOutput.h"
+#include "../AnomalousCouplings/PersonalClasses/interface/MlbStudy.h"
+#include "../AnomalousCouplings/PersonalClasses/interface/TFCreation.h"
+#include "../AnomalousCouplings/PersonalClasses/interface/AnomCoupLight.h"
+#endif
+
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class BTagStudy;
+#pragma link C++ class LHCOOutput;
+#pragma link C++ class MlbStudy;
+#pragma link C++ class TFCreation;
+#pragma link C++ class AnomCoupLight;
+
+#endif
