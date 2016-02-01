0ec1ab175be5eac8bb065c7cfd10a3ce2164eb37
quentin <quentin.python@vub.ac.be>
2016-02-01 11:24:27 +0100

    adapts the displaced leptons functions getter in the run2Selection class

--- Selection/interface/Run2Selection.h
+++ Selection/interface/Run2Selection.h
@@ -88,8 +88,10 @@ public:
     std::vector<TRootElectron*> GetSelectedElectrons(float PTthr, float EtaThr, string WorkingPoint, string ProductionCampaign, bool CutsBased) const;
 
 
-    std::vector<TRootElectron*> GetSelectedDisplacedElectrons (float PtThr, float EtaThr) const;
+    // displaced electrons
+    std::vector<TRootElectron*> GetSelectedDisplacedElectrons (float PtThr, float EtaThr, bool applyIso=true, bool applyId=true) const;
     std::vector<TRootElectron*> GetSelectedDisplacedElectrons () const;
+
 
 
 // MUON GETTERS ==================================================
@@ -98,8 +100,8 @@ public:
 
 
     // displaced muons
-    std::vector<TRootMuon*> GetSelectedDisplacedMuons(float PtThr, float EtaThr, float NormChi2, int NTrackerLayersWithMeas, int NValidMuonHits, int NValidPixelHits, int NMatchedStations, float RelIso) const;
-    std::vector<TRootMuon*> GetSelectedDisplacedMuons(float PtThr, float EtaThr, float RelIs) const;
+    std::vector<TRootMuon*> GetSelectedDisplacedMuons(float PtThr, float EtaThr, float NormChi2, int NTrackerLayersWithMeas, int NValidMuonHits, int NValidPixelHits, int NMatchedStations, float RelIso, bool applyIso=true, bool applyId=true) const;
+    std::vector<TRootMuon*> GetSelectedDisplacedMuons(float PtThr, float EtaThr, float RelIs, bool applyIso=true, bool applyId=true) const;
     std::vector<TRootMuon*> GetSelectedDisplacedMuons() const;
 
 
