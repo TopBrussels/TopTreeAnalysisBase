71090b383da5e37e706febc7e148261b75af068e
quentin <quentin.python@vub.ac.be>
2016-03-15 09:57:31 +0100

    fixes a comment and the indentation

--- Selection/src/JetSelection.cc
+++ Selection/src/JetSelection.cc
@@ -9,13 +9,13 @@ JetSelection::JetSelection()
 
 JetSelection::JetSelection(const std::vector<TRootJet*>& jets_)
 {
-	for(unsigned int i=0; i<jets_.size(); i++) jets.push_back(jets_[i]);
+  for(unsigned int i=0; i<jets_.size(); i++) jets.push_back(jets_[i]);
 }
 
 JetSelection::JetSelection(const JetSelection& s)
 {
-	// copy the objects
-	jets = s.jets;
+  // copy the objects
+  jets = s.jets;
 }
 
 //______________________________________________________________________//
@@ -24,7 +24,7 @@ JetSelection::JetSelection(const JetSelection& s)
 
 JetSelection::~JetSelection()
 {
-	jets.clear();
+  jets.clear();
 }
 
 //____SELECTION GETTERS_________________________________________________//
@@ -34,55 +34,55 @@ JetSelection::~JetSelection()
 // This should be the Standard getter function for Jets in Run-II.  The use of non PF objects should be discouraged.
 std::vector<TRootPFJet*> JetSelection::GetSelectedJets(float PtThr, float EtaThr, bool applyJetID, std::string TightLoose) const
 {
-	std::vector<TRootPFJet*> selectedJets;
-	for(unsigned int i=0; i<jets.size(); i++)
+  std::vector<TRootPFJet*> selectedJets;
+  for(unsigned int i=0; i<jets.size(); i++)
+    {
+      TRootPFJet* init_jet = (TRootPFJet*) jets[i];
+      if (init_jet->jetType() == 2)
 	{
-		TRootPFJet* init_jet = (TRootPFJet*) jets[i];
-		if (init_jet->jetType() == 2)
-		{
-			// PFJets
-			const TRootPFJet* PFJet = static_cast<const TRootPFJet*>(init_jet);
+	  // PFJets
+	  const TRootPFJet* PFJet = static_cast<const TRootPFJet*>(init_jet);
 
-			if( fabs(PFJet->Eta())<EtaThr && PFJet->Pt()>PtThr )
-			{
-				if ( applyJetID )
-				{
-					if(TightLoose == "Loose" && passLoosePFJetID13TeV(PFJet))//This is the 13TeV Recommended Loose PFJet ID.  https://twiki.cern.ch/twiki/bin/viewauth/CMS/JetID
-					{
-						selectedJets.push_back(init_jet);
-					}
-					else if(TightLoose == "Tight" && passTightPFJetID13TeV(PFJet))//This is the 13TeV Recommended Loose PFJet ID.  https://twiki.cern.ch/twiki/bin/viewauth/CMS/JetID
-					{
-						selectedJets.push_back(init_jet);
-					}
-				}
-				else selectedJets.push_back(init_jet);
-			}
+	  if( fabs(PFJet->Eta())<EtaThr && PFJet->Pt()>PtThr )
+	    {
+	      if ( applyJetID )
+		{
+		  if(TightLoose == "Loose" && passLoosePFJetID13TeV(PFJet))//This is the 13TeV Recommended Loose PFJet ID.  https://twiki.cern.ch/twiki/bin/viewauth/CMS/JetID
+		    {
+		      selectedJets.push_back(init_jet);
+		    }
+		  else if(TightLoose == "Tight" && passTightPFJetID13TeV(PFJet))//This is the 13TeV Recommended Tight PFJet ID.  https://twiki.cern.ch/twiki/bin/viewauth/CMS/JetID
+		    {
+		      selectedJets.push_back(init_jet);
+		    }
 		}
+	      else selectedJets.push_back(init_jet);
+	    }
 	}
-	std::sort(selectedJets.begin(),selectedJets.end(),HighestJetPt());
-	return selectedJets;
+    }
+  std::sort(selectedJets.begin(),selectedJets.end(),HighestJetPt());
+  return selectedJets;
 }
 
 std::vector<TRootPFJet*> JetSelection::GetSelectedJets() const
 {
-	return GetSelectedJets(30,2.5,true,"Loose");
+  return GetSelectedJets(30,2.5,true,"Loose");
 }
 
 std::vector<TRootPFJet*> JetSelection::GetSelectedBJets(const std::vector<TRootPFJet*>& seljets, int& btagAlgo, float& btagCut) const
 {
-	std::vector<TRootPFJet*> bjets;
-	Float_t btagDisc = 0;
-	for(unsigned int i = 0; i<seljets.size(); i++)
-	{
-		if(btagAlgo == 1) btagDisc = seljets[i]->btag_combinedInclusiveSecondaryVertexV2BJetTags();
-		else if(btagAlgo == 2) btagDisc = seljets[i]->btag_simpleSecondaryVertexHighEffBJetTags();
-		else if(btagAlgo == 3) btagDisc = seljets[i]->btag_simpleSecondaryVertexHighPurBJetTags();
-		else if(btagAlgo == 4) btagDisc = seljets[i]->btag_combinedSecondaryVertexMVABJetTags();
-		else                   btagDisc = -999999;
-		if(btagDisc>btagCut) bjets.push_back(seljets[i]);
-	}
-	return bjets;
+  std::vector<TRootPFJet*> bjets;
+  Float_t btagDisc = 0;
+  for(unsigned int i = 0; i<seljets.size(); i++)
+    {
+      if(btagAlgo == 1) btagDisc = seljets[i]->btag_combinedInclusiveSecondaryVertexV2BJetTags();
+      else if(btagAlgo == 2) btagDisc = seljets[i]->btag_simpleSecondaryVertexHighEffBJetTags();
+      else if(btagAlgo == 3) btagDisc = seljets[i]->btag_simpleSecondaryVertexHighPurBJetTags();
+      else if(btagAlgo == 4) btagDisc = seljets[i]->btag_combinedSecondaryVertexMVABJetTags();
+      else                   btagDisc = -999999;
+      if(btagDisc>btagCut) bjets.push_back(seljets[i]);
+    }
+  return bjets;
 }
 
 //=======================
@@ -91,75 +91,71 @@ std::vector<TRootPFJet*> JetSelection::GetSelectedBJets(const std::vector<TRootP
 
 bool JetSelection::passLoosePFJetID13TeV(const TRootPFJet* PFJet) const
 {
-	if (fabs(PFJet->Eta()) <= 3.0)
+  if (fabs(PFJet->Eta()) <= 3.0)
+    {
+      if (PFJet->nConstituents() > 1 )
 	{
-		if (PFJet->nConstituents() > 1 )
+	  if (PFJet->neutralHadronEnergyFraction() < 0.99)
+	    {
+	      if (PFJet->neutralEmEnergyFraction() < 0.99 )
 		{
-			if (PFJet->neutralHadronEnergyFraction() < 0.99)
+		  if (fabs(PFJet->Eta()) >= 2.4 || PFJet->chargedEmEnergyFraction() < 0.99 )
+		    {
+		      if (fabs(PFJet->Eta()) >= 2.4 || PFJet->chargedHadronEnergyFraction() > 0)
 			{
-				if (PFJet->neutralEmEnergyFraction() < 0.99 )
-				{
-					if (fabs(PFJet->Eta()) >= 2.4 || PFJet->chargedEmEnergyFraction() < 0.99 )
-					{
-						if (fabs(PFJet->Eta()) >= 2.4 || PFJet->chargedHadronEnergyFraction() > 0)
-						{
-							if (fabs(PFJet->Eta()) >= 2.4 || PFJet->chargedMultiplicity() > 0)
-							{
-								
-									return true;
-								
-							}
-						}
-					}
-				}
+			  if (fabs(PFJet->Eta()) >= 2.4 || PFJet->chargedMultiplicity() > 0)
+			    {
+			      return true;
+			    }
 			}
+		    }
 		}
+	    }
 	}
-	else if (PFJet->neutralEmEnergyFraction() < 0.9 )
+    }
+  else if (PFJet->neutralEmEnergyFraction() < 0.9 )
+    {
+      if(PFJet->neutralMultiplicity() > 10)
 	{
-		if(PFJet->neutralMultiplicity() > 10)
-		{
-			return true;
-		}
+	  return true;
 	}
-	return false;
+    }
+  return false;
 }
 bool JetSelection::passTightPFJetID13TeV(const TRootPFJet* PFJet) const
 {
-	if(fabs(PFJet->Eta()) <= 3.0)
+  if(fabs(PFJet->Eta()) <= 3.0)
+    {
+      if (PFJet->nConstituents() > 1 )
 	{
-		if (PFJet->nConstituents() > 1 )
+	  if (PFJet->neutralHadronEnergyFraction() < 0.9)
+	    {
+	      if (PFJet->neutralEmEnergyFraction() < 0.9 )
 		{
-			if (PFJet->neutralHadronEnergyFraction() < 0.9)
+		  if (fabs(PFJet->Eta()) >= 2.4 || PFJet->chargedEmEnergyFraction() < 0.99 )
+		    {
+		      if (fabs(PFJet->Eta()) >= 2.4 || PFJet->chargedHadronEnergyFraction() > 0)
 			{
-				if (PFJet->neutralEmEnergyFraction() < 0.9 )
-				{
-					if (fabs(PFJet->Eta()) >= 2.4 || PFJet->chargedEmEnergyFraction() < 0.99 )
-					{
-						if (fabs(PFJet->Eta()) >= 2.4 || PFJet->chargedHadronEnergyFraction() > 0)
-						{
-							if (fabs(PFJet->Eta()) >= 2.4 || PFJet->chargedMultiplicity() > 0)
-							{
-								
-									return true;
-								
-							}
-						}
-					}
-				}
+			  if (fabs(PFJet->Eta()) >= 2.4 || PFJet->chargedMultiplicity() > 0)
+			    {
+			      return true;
+			    }
 			}
+		    }
 		}
+	    }
 	}
-	else if (PFJet->neutralEmEnergyFraction() < 0.9 )
+    }
+  else if (PFJet->neutralEmEnergyFraction() < 0.9 )
+    {
+      if(PFJet->neutralMultiplicity() > 10)
 	{
-		if(PFJet->neutralMultiplicity() > 10)
-		{
-			return true;
-		}
+	  return true;
 	}
+    }
 
 
-	return false;
+  return false;
 }
 
 //______________________________________________________________________//
